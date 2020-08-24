8891a76152872406237443c1c0f0f56d06cf871e
Ho Chong Han Nathaniel <e0407282@u.nus.edu>
2020-08-25 00:44:00 +0800

    Add ability to store and list user entered text
    User can now store whatever text entered and display them back when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,7 +6,7 @@ public class Duke {
     }
 
     public static void printHorizontalLinesWithText (String response) {
-        //wraps text in between horizontal lines and prints
+        //Wraps text in between horizontal lines and prints
         printHorizontalLines();
         System.out.println(response);
         printHorizontalLines();
@@ -24,12 +24,33 @@ public class Duke {
         printHorizontalLinesWithText("Top of the mornin' to ya! Name's Natto" + System.lineSeparator()
                 + "Need a pint, two, or somethin' else?");
 
+        String[] reminderList = new String[100];
+        int reminderCount = 0;
         Scanner in = new Scanner(System.in);
         String userCommand = in.nextLine();
         while (!userCommand.equals("bye")) {
-            printHorizontalLinesWithText(userCommand);
+            if (userCommand.toLowerCase().equals("list")) {
+                //Checks if reminderList is empty
+                if (reminderCount == 0) {
+                    printHorizontalLinesWithText("Doing nothing is hard, you never know when you're done");
+                }
+                //Prints reminders
+                else {
+                    printHorizontalLines();
+                    for (int i = 0; i < reminderCount; i++) {
+                        System.out.println(i+1 + ". " + reminderList[i]);
+                    }
+                    printHorizontalLines();
+                    System.out.print(System.lineSeparator());
+                }
+            }
+            else {
+                printHorizontalLinesWithText("Added: " + userCommand);
+                reminderList[reminderCount++] = userCommand;
+            }
             userCommand = in.nextLine();
         }
+        //prints goodbye string after user enters "bye"
         printHorizontalLinesWithText("What, so everyone's supposed to sleep every night now?"
                 + System.lineSeparator() + "You realize that nighttime makes up half of all time?");
     }
