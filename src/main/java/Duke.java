dad750edaaf64a0c155c6769c142fa7285d0f049
Ho Chong Han Nathaniel <e0407282@u.nus.edu>
2020-08-24 23:36:12 +0800

    Add ability to echo commands and exit
    * Duke now echo commands entered by user
    * Exits when user types "bye"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void printHorizontalLines () {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void printHorizontalLinesWithText (String response) {
+        //wraps text in between horizontal lines and prints
+        printHorizontalLines();
+        System.out.println(response);
+        printHorizontalLines();
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         String logo = "  _  _     ___    _____   _____    ___   \n"
                 + " | \\| |   /   \\  |_   _| |_   _|  / _ \\  \n"
@@ -6,12 +20,17 @@ public class Duke {
                 + " |_|\\_|   |_|_|   _|_|_   _|_|_   \\___/  \n"
                 + "_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"| \n"
                 + "\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-' \n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Natto");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(logo);
+        printHorizontalLinesWithText("Top of the mornin' to ya! Name's Natto" + System.lineSeparator()
+                + "Need a pint, two, or somethin' else?");
+
+        Scanner in = new Scanner(System.in);
+        String userCommand = in.nextLine();
+        while (!userCommand.equals("bye")) {
+            printHorizontalLinesWithText(userCommand);
+            userCommand = in.nextLine();
+        }
+        printHorizontalLinesWithText("What, so everyone's supposed to sleep every night now?"
+                + System.lineSeparator() + "You realize that nighttime makes up half of all time?");
     }
 }
