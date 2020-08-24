2f61154780ce256144dc1ab5c0499ba7507dce7e
Ho Chong Han Nathaniel <e0407282@u.nus.edu>
2020-08-24 23:24:57 +0800

    Add user greeting
    Implement an initial skeletal version of the Duke that greets the user and exits

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,5 +7,11 @@ public class Duke {
                 + "_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"| \n"
                 + "\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-' \n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Natto");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
